18868263e06122c684c6b4bd88e7bd77d595c825
bmaple <bmaple@umich.edu>
2015-03-22 20:19:49 -0400

    models should be fixed

--- models.py
+++ models.py
@@ -1,24 +1,18 @@
 from flask import Flask
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
+#from flask.ext.sqlalchemy import ForeignKey
 import Secret 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = Secret.DB_URI
 db = SQLAlchemy(app)
-
-class  DecksCards(db.Model):
-    __tablename__ = "DECKCARDS"
-    deck_id = db.Column(db.Integer, ForeignKey(decks.id), primary_key=True)
-    card_id = db.Column(db.Integer, ForeignKey(Cards.Nid), primary_key=True)
-    def __repr__(self):
-        return '<DECKCARDS %r>' % self.deck_id
 
 
 class Cards(db.Model):
     __tablename__ = "Ncards"
     Nid = db.Column(db.Integer, primary_key = True)
     Nname = db.Column(db.Text)
-    decks = relationship(Decks, secondary='DecksCards')
+    decks = db.relationship('DECKS', secondary='DecksCards')
     def __repr__(self):
         return '<Cards %r>' % self.Nname
 #deck table for a created deck
@@ -26,9 +20,19 @@ class  Decks(db.Model):
     __tablename__ = "DECKS"
     name = db.Column(db.Text, unique=True)
     id = db.Column(db.Integer, primary_key=True)
-    cards = relationship(Cards, secondary='DecksCards')
+    cards = db.relationship(Cards, secondary='DecksCards')
     def __repr__(self):
         return '<DECKS %r>' % self.name
+
+class  DecksCards(db.Model):
+    __tablename__ = "DECKCARDS"
+    deck_id = db.Column(db.Integer, db.ForeignKey('DECKS.id'), primary_key=True)
+    card_id = db.Column(db.Integer, db.ForeignKey('Ncards.Nid'), primary_key=True)
+    def __repr__(self):
+        return '<DECKCARDS %r>' % self.deck_id
+
+
+
 """
 class Sets(db.Model):
     __tablename__ = "Nsets"
